4cba2c84d96a7b809dd6e651f5e448ea779f1668
Hassan Hassnain <ahh.devoders@gmail.com>
2020-04-26 21:36:36 +0500

    Initial Commit

---
+++ Aking2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aking2
+//
+//  Created by Usama Sadiq on 4/26/20.
+//  Copyright © 2020 Usama Sadiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
